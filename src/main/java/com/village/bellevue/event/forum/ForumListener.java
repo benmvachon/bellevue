8b619ca2109cb2963a102a2465daf7f62b85e4d1
Ben Vachon <benmvachon@gmail.com>
2025-05-28 17:41:07 -0400

    fix feed destination uri

--- src/main/java/com/village/bellevue/event/forum/ForumListener.java
+++ src/main/java/com/village/bellevue/event/forum/ForumListener.java
@@ -31,9 +31,9 @@ public class ForumListener {
     Long id = event.getPost().getId();
     if (Objects.isNull(event.getPost().getParent())) {
       messagingTemplate.convertAndSend("/topic/forum/" + forum, id);
-      messagingTemplate.convertAndSendToUser(user.toString(), "/topic/feed/", id);
+      messagingTemplate.convertAndSendToUser(user.toString(), "/topic/feed", id);
       for (Long friend : friendRepository.findFriends(user)) {
-        messagingTemplate.convertAndSendToUser(friend.toString(), "/topic/feed/", id);
+        messagingTemplate.convertAndSendToUser(friend.toString(), "/topic/feed", id);
       }
     }
   }
